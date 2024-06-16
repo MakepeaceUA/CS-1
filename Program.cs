abcd902a069f385cd1e2d4d9ce9cf7d30692bce3
Arsen Sevastyanov <130586920+MakepeaceUA@users.noreply.github.com>
2024-06-16 20:01:30 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace ConsoleApp7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Задание 1
+            //Console.Write("Температуру в цельсиях: ");
+            //int val = Convert.ToInt32(Console.ReadLine());
+            //Console.Write($"Температура в фаренгейтах: {val * 1.8 + 32}");
+
+            //Задание 2
+            //Console.Write("Введите радиус шара: ");
+            //double rad = Convert.ToDouble(Console.ReadLine());
+            //Console.Write($"Объём шара: {(4.0 / 3.0) * 3.14 * Math.Pow(rad, 3)}");
+
+            //Задание 3
+            //Console.Write("Введите стоимость одного ноутбука: ");
+            //int price = Convert.ToInt32(Console.ReadLine());
+            //Console.Write("Введите количество ноутбуков: ");
+            //int quantity = Convert.ToInt32(Console.ReadLine());
+            //Console.Write("Введите процент скидки: ");
+            //int discount = Convert.ToInt32(Console.ReadLine());
+
+            //int amount = price * quantity;
+            //double dis_amount = amount * (discount / 100.0);
+            //double sum = amount - dis_amount;
+
+            //Console.Write($"Сумма заказа: {sum}");
+
+            //Задание 4
+            //Console.Write("Введите длину окружности:");
+
+            //double length = Convert.ToDouble(Console.ReadLine());
+            //double radius = (2 * 3.14) / length;
+            //double area = 3.14 * (radius * radius);
+
+            //Console.WriteLine($"Площадь круга: {area}");
+
+            //Задание 5
+            //Console.Write("Введите 4-значное число:");
+            //int number = Convert.ToInt32(Console.ReadLine());
+
+            //int F_Digit = number / 1000;
+            //int L_Digit = number % 10;
+            //int M_Digits = (number / 10) % 100;
+            //int NewNumber = L_Digit * 1000 + M_Digits * 10 + F_Digit;
+
+            //Console.Write($"Результат: {NewNumber}"); 
+
+            //Задание 6
+            //Console.WriteLine("Введите количество дней:");
+            //int days = Convert.ToInt32(Console.ReadLine());
+
+            //int weeks = days / 7;  
+            //int other_days = days % 7;
+
+            //Console.WriteLine($"{weeks} недель и {other_days} дней.");
+
+            //Задание 7
+            //Console.Write("Введите количество школьников: ");
+            //int N = Convert.ToInt32(Console.ReadLine());
+            //Console.Write("Введите количество яблок: ");
+            //int K = Convert.ToInt32(Console.ReadLine());
+
+            //int PerStudent = K / N; 
+            //int InBasket = K % N; 
+
+            //Console.WriteLine($"Каждому школьнику достанется яблок: {PerStudent}");
+            //Console.WriteLine($"Яблок останется в корзинке: {InBasket}");
+        }
+    }
+}
